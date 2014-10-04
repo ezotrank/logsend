83abf6876352965e5081a92fa136c1e55fdba88f
Maxim Kremenev <ezo@kremenev.com>
2014-10-04 12:27:39 +0700

    add mysql sender

---
+++ logsend/mysql_sender.go
@@ -0,0 +1,111 @@
+package logsend
+
+import (
+	"bytes"
+	"database/sql"
+	"flag"
+	_ "github.com/go-sql-driver/mysql"
+	"strings"
+	"text/template"
+)
+
+// DOCS
+// https://github.com/go-sql-driver/mysql#dsn-data-source-name
+var (
+	mysqlCh    = make(chan *string, 0)
+	mysqlHost  = flag.String("mysql-host", "", "Example: user:password@/database?timeout=30s&strict=true")
+	mysqlQuery = flag.String("mysql-query", "", "Example: insert into test1(teststring, testfloat) values('{{.gate}}', {{.exec_time}});")
+)
+
+func init() {
+	Conf.registeredSenders["mysql"] = &SenderRegister{init: InitMysql, get: NewMysqlSender}
+}
+
+func InitMysql(conf interface{}) {
+	host := conf.(map[string]interface{})["host"].(string)
+	db, err := sql.Open("mysql", host)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	go func() {
+		defer db.Close()
+		Conf.Logger.Println("mysql queue is starts")
+		for query := range mysqlCh {
+
+			err = db.Ping()
+			if err != nil {
+				panic(err.Error()) // proper error handling instead of panic in your app
+			}
+
+			debug("mysql exec query: ", *query)
+			// TODO: exec query with transaction support
+			if Conf.DryRun {
+				continue
+			}
+
+			trx, err := db.Begin()
+			if err != nil {
+				Conf.Logger.Println("mysql init transaction ", err, *query)
+			}
+
+			for _, q := range strings.Split(*query, ";") {
+				if q == "" {
+					continue
+				}
+				if _, err = db.Exec(q + ";"); err != nil {
+					break
+				}
+
+			}
+			if err != nil {
+				trx.Rollback()
+				Conf.Logger.Println("rollback ", err, *query)
+				continue
+			}
+			trx.Commit()
+		}
+	}()
+	return
+}
+
+func NewMysqlSender() Sender {
+	mysqlSender := &MysqlSender{}
+	mysqlSender.sendCh = mysqlCh
+	return Sender(mysqlSender)
+}
+
+type MysqlSender struct {
+	sendCh chan *string
+	tmpl   *template.Template
+}
+
+func (self *MysqlSender) Name() string {
+	return "mysql"
+}
+
+func (self *MysqlSender) SetConfig(rawConfig interface{}) error {
+	var query string
+	switch rawConfig.(map[string]interface{})["query"].(type) {
+	case []interface{}:
+		for _, s := range rawConfig.(map[string]interface{})["query"].([]interface{}) {
+			query = query + s.(string)
+		}
+	case string:
+		query = rawConfig.(map[string]interface{})["query"].(string)
+	}
+	self.tmpl, _ = template.New("query").Parse(query)
+	debug("set config to mysql sender")
+	return nil
+}
+
+func (self *MysqlSender) Send(data interface{}) {
+	buf := new(bytes.Buffer)
+	err := self.tmpl.Execute(buf, data)
+	if err != nil {
+		Conf.Logger.Println("mysql template error ", err, data)
+	}
+	str := buf.String()
+	self.sendCh <- &str
+	return
+}
