0d9713bf42757e79ec4e95ed19b57361c70f199f
Maxim Kremenev <ezo@kremenev.com>
2015-04-26 09:42:15 +0700

    forgot make fmt

--- logsend/system_metrics.go
+++ logsend/system_metrics.go
@@ -1,23 +1,22 @@
 package logsend
 
 import (
-    "time"
-    log "github.com/ezotrank/logger"
-    "github.com/shirou/gopsutil/mem"
-    "github.com/shirou/gopsutil/load"
+	log "github.com/ezotrank/logger"
+	"github.com/shirou/gopsutil/load"
+	"github.com/shirou/gopsutil/mem"
+	"time"
 )
 
 var (
-    SMUpdateInterval = time.Duration(5 * time.Second)
+	SMUpdateInterval = time.Duration(5 * time.Second)
 )
 
-
 func RunSystemMetricsCollect() {
-    for {
-        metrics := make(map[string]interface{},0)
-        metrics["virtual_memory"],_ = mem.VirtualMemory()
-        metrics["cpu_info"],_ = load.LoadAvg() 
-        log.Infoln(metrics)
-        time.Sleep(SMUpdateInterval)
-    }
+	for {
+		metrics := make(map[string]interface{}, 0)
+		metrics["virtual_memory"], _ = mem.VirtualMemory()
+		metrics["cpu_info"], _ = load.LoadAvg()
+		log.Infoln(metrics)
+		time.Sleep(SMUpdateInterval)
+	}
 }
